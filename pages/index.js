8126185fe3e82b6893b3338aa13e2b8c869b9828
videozanmai <yoshi-tnk@ezweb.ne.jp>
2022-01-09 00:24:31 +0900

    3nd mommit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  let title = 'Next.js pddage'
+  let message = 'React Next.js sample page2.'
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+        crossOrigin='anonymous'></link>
+      </Head>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='container'>
+        <h4 className='my-3'>{title}</h4>
+        <div className='alert alert-primary text-center'>
+          <p className='h5'>{message}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
