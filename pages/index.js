2a14747fc8799517f36e72dfae67735d9c25f2f8
videozanmai <yoshi-tnk@ezweb.ne.jp>
2022-01-09 00:43:20 +0900

    4nd mommit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   let title = 'Next.js pddage'
-  let message = 'React Next.js sample page2.'
+  let message = 'React Next.js sample page3.'
 
   return (
     <div>
