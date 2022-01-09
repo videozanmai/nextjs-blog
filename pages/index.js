a670d2cca03bf0541944368b13505869a70f7c6c
videozanmai <yoshi-tnk@ezweb.ne.jp>
2022-01-09 20:46:53 +0900

    6nd mommit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,25 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   let title = 'Next.js pddage'
-  let message = 'React Next.js 動くぞ'
+  let message = 'React Next.js 動く'
+
+  const h1 = {
+    textAlign:'right',
+    padding:'5px 15px'
+  }
+  const p = {
+    textAlign:'left',
+    margin:'0px 5px',
+    color:'#669',
+    fontSize:'18pt'
+  }
+  const subTitle = {
+    textAlign:'center',
+    margin:'0px 5px',
+    color:'#99d',
+    fontSize:'24pt',
+    fontWeight:'bold'
+  }
 
   return (
     <div>
@@ -13,11 +31,11 @@ export default function Home() {
         <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
         crossOrigin='anonymous'></link>
       </Head>
-      <h1 className='bg-primary text-white display-4'>React</h1>
+      <h1 className='bg-primary text-white display-4' style={h1}>React</h1>
       <div className='container'>
-        <h4 className='my-3'>{title}</h4>
+        <h4 className='my-3' style={subTitle}>{title}</h4>
         <div className='alert alert-primary text-center'>
-          <p className='h5'>{message}</p>
+          <p className='h5' style={p}>{message}</p>
         </div>
       </div>
     </div>
